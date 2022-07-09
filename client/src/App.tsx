4fe877b5528f698373e4cf1c61aab191f8364c1d
Marcos Saucedo <saucedoimarcos@gmail.com>
2022-07-09 00:33:36 -0700

    boilerplate created

---
+++ client/src/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import tw from 'twin.macro';
+import styled from 'styled-components'
+
+function App() {
+  return (
+    <div className="App">
+        <Text>
+          Hello World
+        </Text>
+    </div>
+  );
+}
+
+
+const Text = styled.h1`
+
+  ${tw`
+
+
+  `}
+
+`
+
+export default App;
